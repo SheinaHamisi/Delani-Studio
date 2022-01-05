1dc2985721aaceade8f10d5d30d699f35be1a785
SheinaHamisi <hamisisheina@gmail.com>
2022-01-06 00:19:59 +0300

    added hover effect

--- index.js
+++ index.js
@@ -30,6 +30,60 @@ $(document).ready(function() {
     })
 })
 
-function validate(form) {
+$(document).ready(function(){
+    $(".trial").mouseover(function() {
+        $(".square").show();
+    })
+    $(".trial").mouseout(function() {
+        $(".square").hide();
+    })
 
-}
+    $(".trial1").mouseover(function() {
+        $(".square7").show();
+    })
+    $(".trial1").mouseout(function() {
+        $(".square7").hide();
+    })
+
+    $(".trial2").mouseover(function() {
+        $(".square1").show();
+    })
+    $(".trial2").mouseout(function() {
+        $(".square1").hide();
+    })
+
+    $(".trial3").mouseover(function() {
+        $(".square2").show();
+    })
+    $(".trial3").mouseout(function() {
+        $(".square2").hide();
+    })
+
+    $(".trial4").mouseover(function() {
+        $(".square3").show();
+    })
+    $(".trial4").mouseout(function() {
+        $(".square3").hide();
+    })
+
+    $(".trial5").mouseover(function() {
+        $(".square4").show();
+    })
+    $(".trial5").mouseout(function() {
+        $(".square4").hide();
+    })
+
+    $(".trial6").mouseover(function() {
+        $(".square5").show();
+    })
+    $(".trial6").mouseout(function() {
+        $(".square5").hide();
+    })
+    $(".trial7").mouseover(function() {
+        $(".square6").show();
+    })
+    $(".trial7").mouseout(function() {
+        $(".square6").hide();
+    })
+  
+})
